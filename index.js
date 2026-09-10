fce40f7483b55dccd4a74955cb2ddc6ba45dbefc
Manfrds Z <60573289+manfredix64@users.noreply.github.com>
2026-09-10 21:41:55 +0300

    upd

--- index.js
+++ index.js
@@ -8,8 +8,39 @@ async function getData(category) {
 
     const result = await response.json();
     console.log(result);
-    document.getElementById("wrd").innerHTML = result[0].word + " " + result[0].length;
+    document.getElementById("wrd").innerHTML = result[0].word
+    
+    function showCircles() {
+      const container = document.getElementById("health");
+
+      container.innerHTML = ""
+      health = 0
+
+      for (let i = 0; i < result[0].length; i++) {
+        const circle = document.createElement("div");
+        circle.classList.add("circle");
+
+        container.appendChild(circle);
+      }
+    }
+
   } catch (error) {
     console.error(error.message);
   }
+  showCircles()
 }
+
+let health = 0
+
+function addHealth() {
+  const circles = document.querySelectorAll(".circle");
+  
+  if (health < circles.length) {
+    circles[health].classList.add("full");
+    health++;
+  }
+}
+
+
+
+// showCircles()
